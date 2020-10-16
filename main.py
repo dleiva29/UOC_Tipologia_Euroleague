b0670895de37e924a5e52ef11022a25d71bd01a8
dleiva29 <dleiva29@gmail.com>
2020-10-17 01:44:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 """
 Created on Thu Oct 15 19:32:13 2020
 
-@author: Alvaro and David 
+@author: Alvaro and David
 """
 
-print ('Hola Mundo')
-print( 'Prova Alvaro')
+import os
+import pandas as pd
+import requests
+import time
+from bs4 import BeautifulSoup
