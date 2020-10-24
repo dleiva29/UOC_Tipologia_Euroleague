637ea8522839a0c4367cf4590b744ce23ab62d7f
dleiva29 <dleiva29@gmail.com>
2020-10-24 19:20:27 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,12 +28,18 @@ class EuroleagueScraper():
         soup = BeautifulSoup(page.content,features="lxml")
         return soup
     
-    
-      
-      
-      
-
-      
+    def __get_jugadors(self,soup):
+        items=soup.find_all('div', class_='item')
+        for link in items:
+            l=[link.a.get('href')]
+            name_complet=[link.a.get_text().strip()][0]
+            self.links_jugadors=self.links_jugadors.append({"link":l},ignore_index=True)
+            cognom=name_complet.split(", ")[0]
+            nom=name_complet.split(", ")[1]
+            self.jugadors=self.jugadors.append({"name_Complet":name_complet, 
+                                                "cognom":cognom, 
+                                                "nom":nom },ignore_index=True)
+            self.jugadors=self.jugadors[["name_Complet","cognom","nom"]]      
       
       
 
