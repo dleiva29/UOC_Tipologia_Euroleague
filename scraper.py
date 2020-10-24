75a8aeb3df46fad2eff0c86fad3f2b2e548eda4e
dleiva29 <63076986+dleiva29@users.noreply.github.com>
2020-10-24 18:49:30 +0200

    Create scraper.py
    Class file

---
+++ scraper.py
@@ -0,0 +1,15 @@
+  
+## Creación de la clase scraper. Mediante web scraping extrae las estadísticas de los jugadores de la Euroleague de la web 'www.euroleague.net'.
+## Genera dos ficheros .csv, uno con las estadísiticas medias en la Euroleague y otro con las estadísticas desglosadas por temporada. 
+
+
+import os
+import pandas as pd
+import requests
+import time 
+import string
+from bs4 import BeautifulSoup
+
+class EuroleagueScraper():
+  
+  def __init__(self):
